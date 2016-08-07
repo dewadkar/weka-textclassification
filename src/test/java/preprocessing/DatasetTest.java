ea80c4d343fdcfba31a138e8de5b7137f54bbdca
Dnyaneshwar Dewadkar <ddewadka@in.ibm.com>
2016-08-07 22:57:55 +0530

    Dataset test class.

--- src/test/java/preprocessing/DatasetTest.java
+++ src/test/java/preprocessing/DatasetTest.java
@@ -1,7 +1,21 @@
 package preprocessing;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by dewadkar on 8/5/2016.
  */
-public class Dataset {
+public class DatasetTest {
+
+    private String RAW_DATA_FILE_PATH = "resources/rawDataSet/auto.dat.train";
+
+    @Test
+    public void testTextToCSVConvertFunction(){
+        DataSet dataSet= new DataSet();
+
+        assertEquals("",dataSet.createFilesDataSetFromSingleFile(RAW_DATA_FILE_PATH));
+    }
+
 }
