eb43855b0fa7e163b17ea926c2106f89d575dd74
Dnyaneshwar Dewadkar <ddewadka@in.ibm.com>
2016-08-07 22:47:39 +0530

    DataSet methods for specified data.

--- src/main/java/preprocessing/DataSet.java
+++ src/main/java/preprocessing/DataSet.java
@@ -1,16 +1,7 @@
 package preprocessing;
 
 import org.apache.commons.io.FileUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import sun.nio.cs.StandardCharsets;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,57 +12,102 @@ import java.util.regex.Pattern;
  * Created by dewadkar on 8/5/2016.
  */
 public class DataSet {
-    static String DATA_SET_TEST = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\hw2\\auto.dat.test";
-    static String DATA_SET_TRAIN = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\hw2\\auto.dat.test";
+    public static String DATA_SET_TEST = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\hw2\\auto.dat.test";
+    public static String DATA_SET_TRAIN = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\hw2\\auto.dat.train";
+    public static String DATA_SET_TEST_FILES = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\dataset\\testing";
+    public static String DATA_SET_TRAIN_FILES = "C:\\Users\\IBM_ADMIN\\IdeaProjects\\nlp\\resources\\dataset\\training";
 
-    static final Pattern TAG_REGEX = Pattern.compile("(<DOC>)(.+?)(</DOC>)");
 
-    public File createCSV(String raw_data_file_path) {
+    public File createFilesDataSetFromSingleFile(String raw_data_file_path, String dirPath) {
+        List<String> documents = documents(raw_data_file_path);
+        for (String document : documents) {
+            try{
+            createDocumentAsFile(document,dirPath );
 
-        documents();
-        return  null;
-    }
-
-    private void documents() {
-        try {
-            String content = FileUtils.readFileToString(new File(DATA_SET_TEST));
-            List<String> tagValues = new ArrayList<String>();
-            int i =0;
-            int temp=0;
-            while((i=(content.indexOf("<DOC>",i)+1))>0){
-                if(temp==0){
-                    temp = i;
-                }else{
-                    System.out.println(temp+" -- "+i);
-                tagValues.add(content.substring(temp,i));
-                }
+            }catch (Exception e){
+                System.out.println(" -- - ");
             }
-            tagValues.add(content.substring(temp,content.length()));
-            System.out.println(" Total Documents " + tagValues.size());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
 
-    public String fileContent(String fileName) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line);
-            }
-            return sb.toString();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return null;
     }
 
+    private void createDocumentAsFile(String document, String dirPath) {
+        String docID = documentField(document, "DOCID");
+        String product = isCarFord(documentField(document, "PRODUCT")) ? "FORDCAR" : "OTHERCAR";
+        String label = documentText(document, "label", "rating");
+        String docName = docID.trim()+ "_" + product.trim() + "_" + label.trim()+".txt";
+        System.out.println("doc_name "+docName);
+        try {
+            FileUtils.writeStringToFile(new File(dirPath+"/"+label.trim()+"/"+docName.trim()), document, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean isCarFord(String product) {
+        return product.toLowerCase().contains("ford");
+    }
+
+    private String documentField(String document, String tag) {
+        Pattern pattern = Pattern.compile("<" + tag + ">(.+?)</" + tag + ">");
+        Matcher matcher = pattern.matcher(document);
+        matcher.find();
+        return matcher.group(1);
+    }
+
+    private String documentText(String document, String startTag, String endTag) {
+        String matchedText = document.substring(document.indexOf(startTag) + startTag.length()+1, document.indexOf(endTag)-1);
+        return matchedText.trim();
+    }
+
+
+    private List<String> documents(String dataSetFilePath) {
+        try {
+            String content = FileUtils.readFileToString(new File(dataSetFilePath));
+            List<String> tagValues = new ArrayList<String>();
+            int i = 0;
+            int temp = 0;
+            while ((i = (content.indexOf("<DOC>", i) + 1)) > 0) {
+                if (temp == 0) {
+                    temp = i;
+                } else {
+                    tagValues.add(content.substring(temp, i));
+                    temp = i;
+                }
+            }
+            tagValues.add(content.substring(temp, content.length()));
+            System.out.println(" Total Documents " + tagValues.size());
+            return tagValues;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<String>();
+    }
+
     public static void main(String[] args) {
-        DataSet dataSet = new DataSet();
-        dataSet.createCSV(DATA_SET_TRAIN);
+//        DataSet dataSet = new DataSet();
+//        dataSet.createFilesDataSetFromSingleFile(DATA_SET_TRAIN,DATA_SET_TRAIN_FILES);
+//        dataSet.createFilesDataSetFromSingleFile(DATA_SET_TEST,DATA_SET_TEST_FILES);
+
+    }
+
+    public void createTrainFileToDataSetDirForCar() {
+        createFilesDataSetFromSingleFile(DATA_SET_TRAIN,DATA_SET_TRAIN_FILES);
+    }
+
+    public void createTestFileToDataSetDirForCar() {
+        createFilesDataSetFromSingleFile(DATA_SET_TEST,DATA_SET_TEST_FILES);
+    }
+
+    public void createTrainFileToDataSetDirForCarRecommendation() {
+        createFilesDataSetFromSingleFile(DATA_SET_TRAIN,DATA_SET_TRAIN_FILES);
+
+    }
+
+    public void createTestFileToDataSetDirForCarRecommandation() {
+        createFilesDataSetFromSingleFile(DATA_SET_TEST,DATA_SET_TEST_FILES);
+
     }
 }
