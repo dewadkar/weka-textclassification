aa6aa861077b1cf8825ea619a0671102c31e15f8
Dnyaneshwar Dewadkar <ddewadka@in.ibm.com>
2016-08-08 16:16:43 +0530

    Decision Tree J48 Algo end-toend flow.

--- src/main/java/training/TrainTestClassifier.java
+++ src/main/java/training/TrainTestClassifier.java
@@ -5,12 +5,13 @@ import weka.attributeSelection.CfsSubsetEval;
 import weka.attributeSelection.ChiSquaredAttributeEval;
 import weka.attributeSelection.GreedyStepwise;
 import weka.classifiers.Evaluation;
+import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.J48;
 import weka.core.Debug;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 import weka.core.converters.TextDirectoryLoader;
 import weka.filters.Filter;
-import weka.filters.supervised.attribute.AttributeSelection;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
 import java.io.File;
@@ -20,6 +21,8 @@ import java.io.File;
  */
 public class TrainTestClassifier {
 
+    public static String DATA_SET_TRAIN_FILES = "resources/dataset/training/";
+    public static String DATA_SET_TEST_FILES = "resources/dataset/testing";
 
 
     public static void main(String[] args) throws Exception {
@@ -37,12 +40,13 @@ public class TrainTestClassifier {
         System.out.println("Wait ..  loading dataset");
 
         TextDirectoryLoader loader = new TextDirectoryLoader();
-        loader.setDirectory(new File(securlyDataSet.DATA_SET_TRAIN_FILES));
+        loader.setDirectory(new File(DATA_SET_TRAIN_FILES));
         Instances dataRaw = loader.getDataSet();
-//        loader.getOutputFilename();
+
 
         System.out.println("Done ..  loaded dataset");
 
+//        System.exit(0);
         System.out.println("Wait ..  converting data set to word vectors.");
 
         StringToWordVector filter = new StringToWordVector();
@@ -52,17 +56,12 @@ public class TrainTestClassifier {
         System.out.println("Done ..  word vector file like arff file of weka created.");
 
 
-
         System.out.println("Wait ..  processing attribute selection");
 
-        AttributeSelection attributeSelection = new AttributeSelection();  // package weka.filters.supervised.attribute!
+        AttributeSelectedClassifier attributeSelectedClassifier = new AttributeSelectedClassifier();
         CfsSubsetEval eval = new CfsSubsetEval();
         GreedyStepwise search = new GreedyStepwise();
         search.setSearchBackwards(true);
-        attributeSelection.setEvaluator(eval);
-        attributeSelection.setSearch(search);
-        attributeSelection.setInputFormat(dataFiltered);
-        Instances trainingData = Filter.useFilter(dataFiltered,attributeSelection);
 
         System.out.println("Done ..  Selected attributes and created training data set.");
 
@@ -70,39 +69,50 @@ public class TrainTestClassifier {
         System.out.println("Wait ..  Building classifier");
 
         // train J48 and output model
-        J48 baseClassifier = new J48();
-        baseClassifier.buildClassifier(trainingData);
-        System.out.println("\n\nClassifier model:\n\n" + baseClassifier);
+        J48 decisionTreeJ48 = new J48();
+        attributeSelectedClassifier.setClassifier(decisionTreeJ48);
+        attributeSelectedClassifier.setEvaluator(eval);
+        attributeSelectedClassifier.setSearch(search);
+
+
+        decisionTreeJ48.buildClassifier(dataFiltered);
+        System.out.println("\n\nClassifier model:\n\n" + decisionTreeJ48);
         System.out.println("Done ..  Trained classifier");
 
 
         System.out.println("Wait .. Evaluating trained classifier.");
 
-        Evaluation evaluation = new Evaluation(trainingData);
-        evaluation.crossValidateModel(baseClassifier, trainingData, 4, new Debug.Random(1));
+        Evaluation evaluation = new Evaluation(dataFiltered);
+        evaluation.crossValidateModel(decisionTreeJ48, dataFiltered, 4, new Debug.Random(1));
         System.out.println(evaluation.toSummaryString());
         System.out.println("Done ..  Evaluation");
+
 
 
         System.out.println("Wait .. Loading test data set.");
 
         TextDirectoryLoader testLoader = new TextDirectoryLoader();
-        loader.setDirectory(new File(securlyDataSet.DATA_SET_TEST_FILES));
+        testLoader.setDirectory(new File(DATA_SET_TRAIN_FILES));
         Instances testRawData = testLoader.getDataSet();
+
         System.out.println("Done .. Loaded test data set.");
+
+
 
         System.out.println("Wait .. Converting test data set to word vector.");
 
         StringToWordVector testFilter = new StringToWordVector();
-        testFilter.setInputFormat(testRawData);
+        testFilter.setInputFormat(dataRaw);
         Instances testingData = Filter.useFilter(testRawData, testFilter);
 
         System.out.println("Done .. Converted test data set to word vector.");
 
+
+
         System.out.println("Wait .. Evaluating testing data on trained classifier.");
 
-        Evaluation testEvaluation = new Evaluation(trainingData);
-        testEvaluation.evaluateModel(baseClassifier, testingData);
+        Evaluation testEvaluation = new Evaluation(dataFiltered);
+        testEvaluation.evaluateModel(decisionTreeJ48, testingData);
         System.out.println(testEvaluation.toSummaryString("\nResults\n======\n", false));
 
         System.out.println("Exit .. All processing done.");
